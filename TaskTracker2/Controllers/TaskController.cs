f55b64ae4cef7d1b094c0ab31359417ff39a1c6c
Suprunenko <Suprunenko@DESKTOP-S7H3QOE>
2026-05-01 20:03:28 +0200

    Добавьте файлы проекта.

---
+++ TaskTracker2/Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTracker2.Controllers
+{
+    [ApiController]
+    [Route("tasks")]
+    public class TaskController : ControllerBase
+
+    {   private static List<string> tasks = new List<string>();
+        [HttpGet]
+        public List<string> GetTasks()
+        {
+            return tasks;
+        }
+
+        [HttpGet ("add")]
+        public List<string> Add(string task)
+        {
+            tasks.Add(task);
+            return tasks;
+        }
+
+        [HttpPost]
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+    }
+}
