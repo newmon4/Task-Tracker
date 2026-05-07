23af2db405290e541847357eebd10d4284401f2a
Newmon4 <newmon4ik@gmail.com>
2026-05-07 23:35:04 +0200

    Refactor task model for CRUD operations

--- TaskTracker2/Controllers/TaskController.cs
+++ TaskTracker2/Controllers/TaskController.cs
@@ -4,19 +4,30 @@ namespace TaskTracker2.Controllers
 {
     [ApiController]
     [Route("tasks")]
-    public class TaskController : ControllerBase { 
+    public class TaskController : ControllerBase {
 
-         
-        private static List<string> tasks = new List<string>();
+
+        //private static List<string> tasks = new List<string>();
+        public class TaskItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        private static int nextId = 1;
+
+        private static List<TaskItem> tasks = new List<TaskItem>();
+        
         [HttpGet]
-        public List<string> GetTasks()
+        public List<TaskItem> GetTasks()
         {
             return tasks;
         }
 
         [HttpPost ("add")]
-        public string AddTask(string task)
+        public TaskItem AddTask(TaskItem task)
         {
+
+            task.Id = nextId++;
             tasks.Add(task);
             return task;
         }
